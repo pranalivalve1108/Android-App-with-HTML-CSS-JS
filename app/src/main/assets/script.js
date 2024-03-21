004f1a4026100d2dcb2739eaa2879a3a90216673
pranalivalve1108 <pranalivalve2001@gmail.com>
2024-03-21 16:37:03 +0530

    WebApp

---
+++ app/src/main/assets/script.js
@@ -0,0 +1,29 @@
+
+function solve(){
+    document.write("Successfully Registered...!!!");
+}
+
+/*function validateForm(){
+    var x = document.forms["f1"]["username"].value;
+    var y = document.forms["f1"]["password"].value;
+
+    if (x==null){
+    alert("Username can't be blank");
+    return false;
+    }else if(y.length<6){
+    alert("Password must be at least 6 characters long.");
+    return false;
+    }
+}
+checkUsername(){
+
+var username = document.f1.username.value;
+var password = document.f1.password.value;
+    if (username==null || username==""){
+        alert("Username can't be blank");
+        return false;
+    }else if(password.length<6){
+        alert("Password must be at least 6 characters long.");
+        return false;
+  }
+}*/
---
+++ app/src/main/java/com/example/project3_applicationwithhtmlcssjs/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.project3_applicationwithhtmlcssjs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webview;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("file:///android_asset/index.html");
+    }
+}
